eb28ecbcf166f538d4d52f3b1a5ae81e209e7dc9
chuckstory <poowadon.ingeni@gmail.com>
2023-05-02 18:16:52 +0700

    End Of Section 12

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -19,14 +19,35 @@ using IMS.UseCases.Reports;
 using Microsoft.EntityFrameworkCore;
 using IMS.Plugins.EFCoreSqlServer;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// configure authorizations
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireClaim("Department", "Administration"));
+    options.AddPolicy("Inventory", policy => policy.RequireClaim("Department", "InventoryManagement"));
+    options.AddPolicy("Sales", policy => policy.RequireClaim("Department", "Sales"));
+    options.AddPolicy("Purchasers", policy => policy.RequireClaim("Department", "Purchasing"));
+    options.AddPolicy("Productions", policy => policy.RequireClaim("Department", "ProductionManagement"));
+});
 
 
 var constr = builder.Configuration.GetConnectionString("InventoryManagement");
 
 //Configure EF Core for Identity
+builder.Services.AddDbContext<AccountDbContext>(options =>
+{
+    options.UseSqlServer(constr);
+});
+
+//Configure Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<AccountDbContext>();
+
 builder.Services.AddDbContextFactory<IMSContext>(options =>
 {
     options.UseSqlServer(constr);
@@ -86,6 +107,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
